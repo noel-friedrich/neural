7ede7d76ccd1c212e48977e268ca3701d263c5b7
Noel Friedrich <62935655+noel-friedrich@users.noreply.github.com>
2021-02-14 19:12:08 +0100

    Update neuraldisplay.py

--- neuraldisplay.py
+++ neuraldisplay.py
@@ -2,20 +2,20 @@ from tkinter import *
 
 class NeuralDisplay:
 
-    def show(self, nodes, links, size):
+    def show(self, nodes, size):
         root  = Tk()
 
         canvas = Canvas(root, width=size[0], height=size[1])
         canvas.pack()
 
-        max_link = self.get_max(links)
-        for i in range(len(links)):
-            for node in range(len(nodes[i])):
-                for next_node in range(len(nodes[i + 1])): 
-                    pos1 = self.get_node_coords(nodes, i, node, size)
-                    pos2 = self.get_node_coords(nodes, i + 1, next_node, size)
-                    hex_color, w = self.get_color(i, node, links, next_node, max_link)
-                    canvas.create_line(pos1[0], pos1[1], pos2[0], pos2[1], fill=hex_color, width=w)
+        max_link = self.get_max(nodes)
+        for i in range(1, len(nodes)):
+            for k in range(len(nodes[i])):
+                for j in range(len(nodes[i][k]["weights"])):
+                    x1, y1 = self.get_node_coords(nodes, i, k, size)
+                    x2, y2 = self.get_node_coords(nodes, i-1, j, size)
+                    c, w = self.get_color(nodes[i][k]["weights"][j], max_link)
+                    canvas.create_line(x1, y1, x2, y2, fill=c, width=w)
 
         for node, x_pos in zip(nodes, self.get_layer_pos(nodes, size[0])):
             for y_pos in self.get_layer_pos(node, size[1]):
@@ -23,19 +23,21 @@ class NeuralDisplay:
 
         mainloop()
 
-    def get_max(self, links):
+    def get_max(self, nodes):
         maximal = 0
-        for layer in list(links.keys()): 
-            for link in list(links[layer].keys()):
-                if abs(links[layer][link]) > maximal:
-                    maximal = abs(links[layer][link])
+        for i in range(1, len(nodes)):
+            for k in range(len(nodes[i])):
+                for j in range(len(nodes[i][k]["weights"])):
+                    if abs(nodes[i][k]["weights"][j]) > maximal:
+                        maximal = abs(nodes[i][k]["weights"][j])
         return maximal
 
-    def get_color(self, i, node, links, next_node, max_link):
-        value = abs(links["l" + str(i)][str(next_node) + "-" + str(node)])
+    def get_color(self, value, max_link):
         if max_link == 0: max_link = 1
-        percent = 100 * value / max_link
-        out = "#0000" + format(int(percent)*2, '#04x')[2:]
+        percent = 100 * abs(value) / max_link
+        if value > 0: out = "#0000" + format(int(percent)*2, '#04x')[2:]
+        if value <= 0: out = "#" + format(int(percent)*2, '#04x')[2:] + "0000"
+        print(out)
         return out, percent / 15
 
     def get_node_coords(self, nodes, i, node, size):
